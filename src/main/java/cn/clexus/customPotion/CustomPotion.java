5800457b2985a112a74dfe743ff8b3b6d70e4f97
Clexus <528373858@qq.com>
2025-06-19 18:09:51 +0800

    update

--- src/main/java/cn/clexus/customPotion/CustomPotion.java
+++ src/main/java/cn/clexus/customPotion/CustomPotion.java
@@ -4,8 +4,6 @@ import cn.clexus.customPotion.commands.CommandCompleter;
 import cn.clexus.customPotion.commands.CommandHandler;
 import cn.clexus.customPotion.effects.CustomEffectType;
 import cn.clexus.customPotion.events.EventsListener;
-import cn.clexus.customPotion.events.PacketsListener;
-import cn.clexus.customPotion.events.PlayerCollideEvent;
 import cn.clexus.customPotion.hooks.mythicmobs.MythicMobsEvents;
 import cn.clexus.customPotion.hooks.mythicmobs.MythicMobsSupport;
 import cn.clexus.customPotion.hooks.placeholderapi.PlaceholderAPISupport;
@@ -14,32 +12,21 @@ import cn.clexus.customPotion.managers.PotionManager;
 import cn.clexus.customPotion.utils.EffectLoader;
 import cn.clexus.customPotion.utils.I18n;
 import com.github.retrooper.packetevents.PacketEvents;
-import com.github.retrooper.packetevents.event.PacketListenerPriority;
 import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.util.BoundingBox;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.logging.Level;
 
 public final class CustomPotion extends JavaPlugin {
-    private final Map<Player, Set<Entity>> playerCollisions = new HashMap<>();
     public static DatabaseManager databaseManager;
     public static CustomPotion plugin;
 
@@ -66,13 +53,11 @@ public final class CustomPotion extends JavaPlugin {
         PotionManager.startEffectProcessor(this);
         CustomEffectType.initializeStaticFields();
         Bukkit.getPluginManager().registerEvents(new EventsListener(), this);
-        PacketEvents.getAPI().getEventManager().registerListener(new PacketsListener(), PacketListenerPriority.NORMAL);
         if (MythicMobsSupport.hasSupport()) {
             Bukkit.getPluginManager().registerEvents(new MythicMobsEvents(), this);
         }
         this.getCommand("customeffect").setExecutor(new CommandHandler());
         this.getCommand("customeffect").setTabCompleter(new CommandCompleter());
-        new CollisionDetectionTask().runTaskTimer(this, 0L, 10L);
         try {
             databaseManager = new DatabaseManager();
         } catch (SQLException e) {
@@ -98,42 +83,5 @@ public final class CustomPotion extends JavaPlugin {
         PacketEvents.getAPI().terminate();
         PotionManager.shutdown();
         databaseManager.shutdown();
-    }
-
-    private class CollisionDetectionTask extends BukkitRunnable {
-        private static final double RADIUS = 1.8;
-
-        @Override
-        public void run() {
-            playerCollisions.keySet().removeIf(player -> !player.isOnline());
-
-            for (Player player : Bukkit.getOnlinePlayers()) {
-                Set<Entity> currentCollisions = new HashSet<>();
-
-                for (Entity e : player.getNearbyEntities(RADIUS, RADIUS, RADIUS)) {
-                    if (!(e instanceof LivingEntity entity)) {
-                        continue;
-                    }
-
-                    BoundingBox playerBox = player.getBoundingBox();
-                    BoundingBox entityBox = entity.getBoundingBox();
-                    if (playerBox.overlaps(entityBox)) {
-                        currentCollisions.add(entity);
-
-                        Set<Entity> playerCollisionSet = playerCollisions.computeIfAbsent(player, k -> new HashSet<>());
-
-                        if (!playerCollisionSet.contains(entity)) {
-                            Bukkit.getPluginManager().callEvent(new PlayerCollideEvent(player, entity));
-                            playerCollisionSet.add(entity);
-                        }
-                    }
-                }
-
-                Set<Entity> existingCollisions = playerCollisions.get(player);
-                if (existingCollisions != null) {
-                    existingCollisions.retainAll(currentCollisions);
-                }
-            }
-        }
     }
 }
